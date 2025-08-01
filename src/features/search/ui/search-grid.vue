2ed9cc7f9eb6862be50c8eb092649b646c148946
Axrorbek <axrorbeknis@gmail.com>
2025-08-01 16:37:36 +0500

    added search page

---
+++ src/features/search/ui/search-grid.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { SearchInput, SearchResultItem, useSearchService } from "~/features/search"
+import { useSearchStore } from "~/entities/search"
+
+const searchStore = useSearchStore()
+const { debouncedFilterSearchList } = useSearchService()
+const { items, params } = storeToRefs(searchStore)
+const { t } = useI18n({ useScope: "local" })
+</script>
+
+<template>
+  <section class="container-7xl">
+    <div class="p-section">
+      <search-input v-model="params.keyword" @update:model-value="debouncedFilterSearchList" />
+      <div v-if="items?.events">
+        <h3 class="mb-4 mt-10 text-sm font-bold uppercase text-gray-600">
+          {{ t("events") }}
+        </h3>
+        <div v-for="page in items.events" class="flex flex-col gap-6" :key="page.title">
+          <search-result-item :title="page.title" :link="page.link" :description="page.description" />
+        </div>
+      </div>
+      <div v-if="items?.posts">
+        <h3 class="mb-4 mt-10 text-sm font-bold uppercase text-gray-600">
+          {{ t("posts") }}
+        </h3>
+        <div v-for="page in items.pages" class="flex flex-col gap-6" :key="page.title">
+          <search-result-item
+            :title="page.title"
+            :link="page.link"
+            :author="item.author"
+            :description="page.description"
+          />
+        </div>
+      </div>
+      <div v-if="items?.gallery">
+        <h3 class="mb-4 mt-10 text-sm font-bold uppercase text-gray-600">
+          {{ t("gallery") }}
+        </h3>
+        <div v-for="page in items.gallery" class="flex flex-col gap-6" :key="page.title">
+          <search-result-item :title="page.title" :link="page.link" :description="page.description" />
+        </div>
+      </div>
+      <div v-if="items?.pages">
+        <h3 class="mb-4 mt-10 text-sm font-bold uppercase text-gray-600">
+          {{ t("pages") }}
+        </h3>
+        <div v-for="page in items.pages" class="flex flex-col gap-6" :key="page.title">
+          <search-result-item :title="page.title" :link="page.link" :description="page.description" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<i18n lang="json">
+{
+  "en": {
+    "pages": "Pages",
+    "gallery": "Gallery",
+    "events": "Events",
+    "posts": "News and Insights"
+  },
+  "ru": {
+    "pages": "Страницы",
+    "gallery": "Галерея",
+    "events": "События",
+    "posts": "Новости и аналитика"
+  },
+  "uz": {
+    "pages": "Sahifalar",
+    "gallery": "Galereya",
+    "events": "Tadbirlar",
+    "posts": "Yangiliklar va tahlillar"
+  }
+}
+</i18n>
---
+++ src/features/search/ui/search-input.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const model = defineModel()
+</script>
+
+<template>
+  <div class="relative">
+    <div class="absolute bottom-0 left-0 top-0 grid w-12 place-items-center">
+      <Icon class="text-2xl text-gray-600" name="lucide:search" />
+    </div>
+
+    <input v-bind="$attrs" v-model="model" class="form-input h-12 w-full !pl-12" placeholder="Search..." type="text" />
+  </div>
+</template>
