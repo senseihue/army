369482513dc21b0d65e1c2b668a3f9bb2621b210
Umarxon <xakimxonovumarxon@gmail.com>
2025-08-07 15:38:43 +0500

    model add and api changed

--- src/features/service/integration/service-integration.api.ts
+++ src/features/service/integration/service-integration.api.ts
@@ -1,17 +1,17 @@
 export const useServiceIntegrationApi = () => {
-  const BASE_URL = "/integration"
+  const BASE_URL = "/integration/public/integration"
   const { $http } = useNuxtApp()
 
   const getEAuctionList = async (params: Record<string, any>): AsyncResponseContainer<any[]> => {
-    return $http.$get(`${BASE_URL}/iskm/eauksion/lots`, { params })
+    return $http.$get(`${BASE_URL}/eauksion/lot`, { params })
   }
 
   const getEAuctionById = async (lot_id: number): AsyncResponseContainer => {
-    return $http.$get(`${BASE_URL}/iskm/eauksion/lot-info`, { params: { lot_id } })
+    return $http.$get(`${BASE_URL}/iskm/lot/${lot_id}`)
   }
 
   const getUzExList = async (params: Record<string, any>): AsyncResponseContainer<any[]> => {
-    return $http.$get(`${BASE_URL}/uz-ex`, { params })
+    return $http.$get(`${BASE_URL}/iskm/sell-products`, { params })
   }
 
   const getUzExById = async (id: number): AsyncResponseContainer => {
