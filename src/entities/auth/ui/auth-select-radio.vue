d9460839c8731e032bb18373172bf9be689af936
Sirojiddin <sirojiddin.rikhsiboyev@gmail.com>
2025-08-09 10:43:55 +0500

    +

--- src/entities/auth/ui/auth-select-radio.vue
+++ src/entities/auth/ui/auth-select-radio.vue
@@ -1,23 +1,30 @@
 <script setup lang="ts">
-
 interface Props {
-    logo: string
-    label: string
-    value: string
+  image: string
+  label: string
+  value: string
 }
 
-const model = defineModel<any>()
 defineProps<Props>()
+const model = defineModel<any>()
 </script>
+
 <template>
-    <label :for="value">
-        <input v-model="model" :id="value" class="hidden" type="radio" :value="value" />
-        <div class="flex cursor-pointer flex-row items-center gap-4 rounded-lg border-2 border-blue-light p-4 sm:flex-col sm:justify-center sm:gap-2 sm:p-10"
-            :class="model === value && '!border-blue-midnight'">
-            <!-- <img :src="`../../../public/img/auth/${value}.png`" :alt="label" /> -->
-            <!-- <slot name="logo" /> -->
-            <img :src="logo" :alt="label" />
-            <p>{{ label }}</p>
-        </div>
-    </label>
+  <label class="auth-select-radio" :for="value">
+    <input v-model="model" class="hidden" type="radio" :id="value" :value="value" />
+    <img :src="image" :alt="label" />
+    <span>{{ label }}</span>
+  </label>
 </template>
+
+<style scoped>
+.auth-select-radio {
+  &:has(input[type="radio"]:checked) {
+    @apply  border-blue-midnight ring ring-blue-midnight/20;
+  }
+
+  @apply flex flex-row items-center gap-4 sm:flex-col sm:justify-center p-4 sm:gap-2 sm:p-10;
+  @apply rounded-2xl border-2 border-blue-light cursor-pointer hover:border-blue-midnight/60;
+  @apply transition-colors duration-300 ease-in-out;
+}
+</style>
--- src/features/auth/ui/auth-role-select-modal.vue
+++ src/features/auth/ui/auth-role-select-modal.vue
@@ -3,105 +3,76 @@ import { useAuthService } from "~/features/auth"
 import { AuthSelectRadio } from "~/entities/auth/ui"
 
 class AuthRole {
-    type: "investor" | "entrepreneur" = 'entrepreneur'
-    residency: "resident" | "not_resident" = "resident"
+  type: "investor" | "entrepreneur" = "entrepreneur"
+  residency: "resident" | "not_resident" = "resident"
+
+  isInvestor() {
+    return this.type === "investor"
+  }
+
+  isResident() {
+    return this.residency === "resident"
+  }
 }
 
-const localePath = useLocalePath()
 const modal = useModal()
+const localePath = useLocalePath()
 
 const { getRedirectUrl } = useAuthService()
 
 const form = ref<AuthRole>(new AuthRole())
 
 const signIn = () => {
-    modal.hide("auth-role-select-modal")
-    if (form.value.residency === "resident") getRedirectUrl(`${window.location.origin}/sso`)
-    else navigateTo(localePath("/auth/sign-in"))
+  modal.hide("auth-role-select-modal")
+  if (form.value.isResident()) getRedirectUrl(`${window.location.origin}/sso`)
+  else navigateTo(localePath("/auth/sign-in"))
 }
 
 const signUp = () => {
-    modal.hide("auth-role-select-modal")
-    if (form.value.type === "investor") navigateTo(localePath(`/guide/register-investor?type=${form.value.type}`))
-    else navigateTo(localePath(`/guide/register-business?type=${form.value.residency}`))
+  modal.hide("auth-role-select-modal")
+  if (form.value.isInvestor()) navigateTo(localePath(`/guide/register-investor?type=${form.value.residency}`))
+  else navigateTo(localePath(`/guide/register-business?type=${form.value.residency}`))
 }
 </script>
 <template>
-    <ui-modal id="auth-role-select-modal">
-        <div class="grid grid-cols-1 gap-2 p-4 sm:grid-cols-2 sm:gap-4">
-            <!-- TYPE -->
-            <h2 class="text-left text-xl sm:col-span-2 sm:text-center">{{ $t("labels.select_type") }}</h2>
+  <ui-modal id="auth-role-select-modal" :label="$t('labels.select_type')">
+    <div class="grid grid-cols-1 gap-2 p-4 sm:grid-cols-2 sm:gap-4">
+      <auth-select-radio
+        v-model="form.type"
+        value="investor"
+        image="/img/auth/investor.png"
+        :label="$t('actions.investor')"
+      />
 
-            <!-- TODO: Create reusable component for radio buttons -->
-            <auth-select-radio 
-                v-model="form.type" 
-                value="investor" 
-                logo="/img/auth/investor.png"
-                :label="$t('actions.investor')" 
-            >
-            </auth-select-radio>
-            <auth-select-radio 
-                v-model="form.type" 
-                value="entrepreneur" 
-                logo="/img/auth/entrepreneur.png"
-                :label="$t('actions.entrepreneur')" 
-            />
-            <!-- <label for="investor">
-                <input v-model="form.type" id="investor" class="hidden" type="radio" value="investor" />
-                <div class="flex cursor-pointer flex-row items-center gap-4 rounded-lg border-2 border-blue-light p-4 sm:flex-col sm:justify-center sm:gap-2 sm:p-10"
-                    :class="form.type === 'investor' && '!border-blue-midnight'">
-                    <img src="~/public/img/auth/investor.png" alt="Investor" />
-                    <p>{{ $t("actions.investor") }}</p>
-                </div>
-            </label>
+      <auth-select-radio
+        v-model="form.type"
+        value="entrepreneur"
+        image="/img/auth/entrepreneur.png"
+        :label="$t('actions.entrepreneur')"
+      />
 
-            <label for="entrepreneur">
-                <input v-model="form.type" id="entrepreneur" class="hidden" type="radio" value="entrepreneur" />
-                <div class="flex cursor-pointer flex-row items-center gap-4 rounded-lg border-2 border-blue-light p-4 sm:flex-col sm:justify-center sm:gap-2 sm:p-10"
-                    :class="form.type === 'entrepreneur' && '!border-blue-midnight'">
-                    <img src="~/public/img/auth/entrepreneur.png" alt="Entrepreneur" />
-                    <p>{{ $t("actions.entrepreneur") }}</p>
-                </div>
-            </label> -->
+      <h2 class="text-xl font-semibold leading-8 sm:col-span-2">{{ $t("labels.select_residency") }}</h2>
 
-            <!-- RESIDENT -->
-            <h2 class="mt-4 text-left text-xl sm:col-span-2 sm:text-center">{{ $t("labels.select_residency") }}</h2>
+      <auth-select-radio
+        v-model="form.residency"
+        value="resident"
+        image="/img/auth/resident.png"
+        :label="$t('actions.resident')"
+      />
 
-             <auth-select-radio 
-                v-model="form.residency" 
-                value="resident" 
-                logo="/img/auth/resident.png"
-                :label="$t('actions.resident')" 
-            />
-             <auth-select-radio 
-                v-model="form.residency" 
-                value="not_resident" 
-                logo="/img/auth/not_resident.png"
-                :label="$t('actions.non_resident')" 
-            />
-            <!-- <label for="resident">
-                <input v-model="form.residency" id="resident" class="hidden" type="radio" value="resident" />
-                <div class="flex cursor-pointer flex-row items-center gap-4 rounded-lg border-2 border-blue-light p-4 sm:flex-col sm:justify-center sm:gap-2 sm:p-10"
-                    :class="form.residency === 'resident' && '!border-blue-midnight'">
-                    <img src="~/public/img/auth/resident.png" alt="Resident" />
-                    <p>{{ $t("actions.resident") }}</p>
-                </div>
-            </label>
+      <auth-select-radio
+        v-model="form.residency"
+        value="not_resident"
+        image="/img/auth/not_resident.png"
+        :label="$t('actions.non_resident')"
+      />
+    </div>
 
-            <label for="not_resident">
-                <input v-model="form.residency" id="not_resident" class="hidden" type="radio" value="not_resident" />
-                <div class="flex cursor-pointer flex-row items-center gap-4 rounded-lg border-2 border-blue-light p-4 sm:flex-col sm:justify-center sm:gap-2 sm:p-10"
-                    :class="form.residency === 'not_resident' && '!border-blue-midnight'">
-                    <img src="~/public/img/auth/not_resident.png" alt="Not-resident" />
-                    <p>{{ $t("actions.non_resident") }}</p>
-                </div>
-            </label> -->
-        </div>
-        <template #footer="{ hide }">
-            <div class="flex w-full justify-center gap-2 py-4">
-                <ui-button color="success" :label="$t('actions.sign_in')" @click="signIn" />
-                <ui-button color="primary" :label="$t('actions.sign_up')" @click="signUp" />
-            </div>
-        </template>
-    </ui-modal>
+    <template #footer>
+      <div class="ui-modal-footer">
+        <ui-button rounded color="primary" :label="$t('actions.sign_up')" @click="signUp" />
+        <ui-button rounded color="success" :label="$t('actions.sign_in')" @click="signIn" />
+      </div>
+    </template>
+  </ui-modal>
 </template>
